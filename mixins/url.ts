de36f1c3e4810a45d03d520654112fb82865833b
Johannes <johannes.mainusch@kommitment.biz>
2023-01-28 18:09:37 +0100

    Updates

--- mixins/url.ts
+++
@@ -1,6 +0,0 @@
-/* the spreadesheet url */
-const appConfig = useAppConfig()
-const GURL = 'https://docs.google.com/spreadsheets/d/'
-const GdataUrl = GURL + appConfig.GKEY
-
-export const URL = GdataUrl
