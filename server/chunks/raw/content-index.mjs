d4a5587f9480051f52643c52ea083e8623577e53
Johannes <johannes.mainusch@kommitment.biz>
2023-01-30 20:39:33 +0100

    Updates

--- server/chunks/raw/content-index.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const contentIndex = "{\"/2023-pechakucha\":[\"content:2023-pechakucha.md\"]}";
-
-export { contentIndex as default };
-//# sourceMappingURL=content-index.mjs.map
--- server/chunks/raw/content-navigation.mjs
+++
@@ -1,5 +0,0 @@
-// ROLLUP_NO_REPLACE 
- const contentNavigation = "[{\"title\":\"PechaKucha\",\"_path\":\"/2023-pechakucha\"}]";
-
-export { contentNavigation as default };
-//# sourceMappingURL=content-navigation.mjs.map
