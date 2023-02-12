a5387a0f3b36fc7f90a3cc26960844eb663e1a13
Johannes <johannes.mainusch@kommitment.biz>
2023-02-12 21:45:18 +0100

    Updates

---
+++ _nuxt/config.672fa66d.js
@@ -0,0 +1,1 @@
+import{J as n,j as o,K as a}from"./entry.f8bb709e.js";function f(){const p=n();return p._appConfig||(p._appConfig=o(a)),p._appConfig}export{f as u};
--- _nuxt/config.946f49aa.js
+++
@@ -1,1 +0,0 @@
-import{J as n,j as o,K as a}from"./entry.c67d78e3.js";function f(){const p=n();return p._appConfig||(p._appConfig=o(a)),p._appConfig}export{f as u};
