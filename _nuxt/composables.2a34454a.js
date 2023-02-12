9e4974d0c8e7732d765f3371d67eaa819eac5050
Johannes <johannes.mainusch@kommitment.biz>
2023-02-12 21:57:26 +0100

    Updates

--- _nuxt/composables.2a34454a.js
+++
@@ -1,1 +0,0 @@
-import{J as r}from"./entry.f8bb709e.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ _nuxt/composables.eb240c5f.js
@@ -0,0 +1,1 @@
+import{J as r}from"./entry.b325d7e8.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
